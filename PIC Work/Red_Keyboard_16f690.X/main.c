6d915a1aa0c4bc58859e9556e812ed0c4392ccab
James Hagerman <james.hagerman@gmail.com>
2015-05-07 09:47:46 -0700

    Starting to add EEPROM storage of the tuning values.

--- PIC Work/Red_Keyboard_16f690.X/main.c
+++ PIC Work/Red_Keyboard_16f690.X/main.c
@@ -87,13 +87,12 @@ unsigned char SPI_Read(unsigned char addr)
 #define MultBS1 PORTCbits.RC3
 #define MultBS2 PORTCbits.RC4
 
-uint16_t key_count = 23;
+static uint16_t key_count = 23;
 uint16_t pressed_keys = 0;
 uint16_t last_key = 0;
+uint8_t current_key = 0;
 
-// Tuning mode:
-bool in_tuning_mode = false;
-
+// Check a key and return true if it's currently pressed
 bool checkKey(uint8_t keyVal) {
    keyVal = (key_count - keyVal - 1); // Flip the index
    if (keyVal <= 15) {
@@ -121,17 +120,6 @@ bool checkKey(uint8_t keyVal) {
     return false;
 }
 
-bool tuningCheck() {
-    // This will check to see if the highest and lowest keys are currently pressed
-    // If so, the program will enter a "tuning mode" that will allow offsets to
-    // be written into the chips EEPROM.
-    if (checkKey(0) && checkKey(22)) {
-        in_tuning_mode = true;
-    }
-}
-
-
-
 // take a keyCode between 0-22 and convert it to a number between 0-4095
 uint32_t get_voltage(uint32_t keyCode) {
     //NewValue = (((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin
@@ -148,6 +136,107 @@ uint32_t get_voltage(uint32_t keyCode) {
     return (keyCode * 1502) / (key_count-1); 
 }
 
+// Tuning mode:
+bool in_tuning_mode = false;
+int8_t currently_tuning = 0;
+uint16_t tuning[23];
+void tuningCheck() {
+    // This will check to see if the two highest lowest keys are currently pressed
+    // If so, the program will enter a "tuning mode" that will allow offsets to
+    // be written into the chips EEPROM.
+    if (checkKey(21) && checkKey(22)) {
+        in_tuning_mode = true;
+        printf("\n\rEntering tuning mode...\n\r");
+    }
+}
+void load_tuning() {
+    for (current_key = 0; current_key < key_count; current_key += 1) {
+        tuning[current_key] = eeprom_read(current_key);
+    }
+}
+void print_tuning() {
+    for (current_key = 0; current_key < key_count; current_key += 1) {
+        printf("Note: %i\t Value: %i\n\r",current_key, tuning[current_key]);
+    }
+}
+void save_tuning() {
+    for (current_key = 0; current_key < key_count; current_key += 1) {
+        uint16_t value = tuning[current_key];
+        printf("Writing value: %i", value);
+        eeprom_write(current_key, value);
+        while(WR){
+            printf(".");
+        }
+        printf("Done!\n\r");
+    }
+}
+void tune_down() {
+    
+}
+void tune_up() {
+    
+}
+void tune() {
+    __delay_ms(100); // Slow this mode down so we don't get double ui interaction
+    // Loop through the keys to allow us to manage the tuning mode
+    for (current_key = 0; current_key < key_count; current_key += 1) {
+        if (checkKey(current_key)) {
+            // If lowest key on keyboard is pressed, exit tuning mode:
+            if (current_key == 0) {
+                in_tuning_mode = false;
+                break;
+            }
+
+            // Pick which key we're currently tuning
+            if (current_key == 19) {
+                currently_tuning += 1;
+            }
+            if (current_key == 17) {
+                currently_tuning -= 1;
+            }
+            
+            // Actually tune the key:
+            if (current_key == 18) {
+                tune_down();
+            }
+            if (current_key == 20) {
+                tune_up();
+            }
+
+        } 
+    }
+    if (currently_tuning < 0) {
+        currently_tuning = 0;
+    } else if (currently_tuning > key_count-1) {
+        currently_tuning = key_count-1;
+    }
+}
+
+void play() {
+    pressed_keys = 0;
+    last_key = 100;
+    for (current_key = 0; current_key < key_count; current_key += 1) {
+
+        if (checkKey(current_key)) {
+            pressed_keys += 1;
+            last_key = current_key;
+        } 
+
+    }
+
+    if (pressed_keys > 0) {
+        PORTCbits.RC5 = 1; // Gate on
+
+        uint16_t real_value = get_voltage(last_key); 
+        printf("Last Key: %i \t Real Value: %i  \t Pressed Keys: %i \n\r", last_key, real_value, pressed_keys);
+        SPI_Write(real_value);
+    } else {
+        PORTCbits.RC5 = 0; // Gate off
+    }
+}
+
+
+
 /******************************************************************************/
 /* Main Program                                                               */
 /******************************************************************************/
@@ -159,20 +248,27 @@ void main(void)
     /* Initialize I/O and Peripherals for application */
     InitApp();
     
+    printf("\n\rRed Keyboard by James Hagerman 2015\n\r");
+    
+    // Load and print tuning values stored in EEPROM:
+    load_tuning();
+    print_tuning();
+    
+    tuning[10] = 100;
+    
+    // Save the tuning as a test:
+    save_tuning();
+    
     // Check to see if we should enter tuning mode:
     tuningCheck();
     while(in_tuning_mode) {
-        printf("In tuning mode\n\r");
-        
-        // If F in the middle of the keyboard is pressed, exit tuning mode:
-        if (checkKey(10)) {
-            in_tuning_mode = false;
-        }
+        tune();
     }
 
     __delay_ms(1000);
     while(1)
     {
+        play();
         // Serial UART test:
 //        printf("This is a test!\n");
 
@@ -211,30 +307,6 @@ void main(void)
 //        } else {
 //            PORTCbits.RC5 = 0;
 //        }
-        
-        pressed_keys = 0;
-        last_key = 100;
-        for (uint16_t current_key = 0; current_key < key_count; current_key += 1) {
-             
-            if (checkKey(current_key)) {
-                pressed_keys += 1;
-                last_key = current_key;
-            }
-
-        }
-        
-        if (pressed_keys > 0) {
-            PORTCbits.RC5 = 1; // Gate on
-            
-            uint16_t real_value = get_voltage(last_key); 
-            printf("Last Key: %i \t Real Value: %i  \t Pressed Keys: %i \n\r", last_key, real_value, pressed_keys);
-            SPI_Write(real_value);
-        } else {
-            PORTCbits.RC5 = 0; // Gate off
-        }
-        // End keyboard input
-        //=============
-        
     }
 
 }
